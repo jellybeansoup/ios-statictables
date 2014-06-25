24feecdcb313a032496509117c8a750e9c2e6f9d
Daniel Farrelly <daniel@jellystyle.com>
2014-06-25 19:37:40 +1000

    Removing and ignoring docs folder.

---
+++ src/StaticTables/JSMStaticRow.m
@@ -0,0 +1,13 @@
+//
+//  JSMStaticRow.m
+//  StaticTables
+//
+//  Created by Daniel Farrelly on 20/06/2014.
+//  Copyright (c) 2014 Daniel Farrelly. All rights reserved.
+//
+
+#import "JSMStaticRow.h"
+
+@implementation JSMStaticRow
+
+@end
