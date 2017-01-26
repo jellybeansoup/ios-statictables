8ee36f86360045774e5d5c6777fd75e3eebc6277
Daniel Farrelly <daniel@jellystyle.com>
2017-01-27 09:57:04 +1100

    Update year of copyright.

--- src/StaticTables/JSMStaticBooleanPreference.h
+++ src/StaticTables/JSMStaticBooleanPreference.h
@@ -1,5 +1,5 @@
 //
-// Copyright © 2014 Daniel Farrelly
+// Copyright © 2017 Daniel Farrelly
 //
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
